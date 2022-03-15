91e5637f36086ee7f0395eff2f791239fbbb126a
Morak <d.a.brovchenko@gmail.com>
2022-03-15 18:16:32 +0900

    AddUseCaseToDomain

---
+++ app/src/main/java/com/example/shoppinglist/domain/UpgradeShopItemUseCase.kt
@@ -0,0 +1,7 @@
+package com.example.shoppinglist.domain
+
+class UpgradeShopItemUseCase {
+    fun upgradeShopItem(shopItem: ShopItem) {
+
+    }
+}
---
+++ app/src/main/java/com/example/shoppinglist/domain/getShopItemUseCase.kt
@@ -0,0 +1,7 @@
+package com.example.shoppinglist.domain
+
+class getShopItemUseCase {
+    fun getShopItem(shopItemID: Int): ShopItem{
+        TODO()
+    }
+}
