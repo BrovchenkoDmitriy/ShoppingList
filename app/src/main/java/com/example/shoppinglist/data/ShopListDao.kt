09b1a3915e1cae4d626efb9dc4e4ee8d1581d838
BrovchenkoDmitriy <99586855+BrovchenkoDmitriy@users.noreply.github.com>
2022-10-21 11:42:38 +0900

    add data and ContentValues. delete data and Selection, SelectionArgs

--- app/src/main/java/com/example/shoppinglist/data/ShopListDao.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListDao.kt
@@ -16,8 +16,14 @@ interface ShopListDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun addShopItem(shopItemDbModel: ShopItemDbModel)
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun addShopItemContentProvider(shopItemDbModel: ShopItemDbModel)
+
     @Query("DELETE FROM shop_items WHERE id = :shopItemID")
     suspend fun deleteShopItem(shopItemID: Int)
+
+    @Query("DELETE FROM shop_items WHERE id = :shopItemID")
+    fun deleteShopItemSycn(shopItemID: Int):Int
 
     @Query("SELECT * FROM shop_items WHERE id = :shopItemID LIMIT 1")
     suspend fun getShopItem(shopItemID: Int): ShopItemDbModel
